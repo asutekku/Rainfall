f64f3f80fb9408e887b9191f831451d6544d6c00
asutekku <asutekku@protonmail.com>
2018-07-10 23:02:53 +0300

    Refactored Movement.ts

--- src/ts/interact/Movement.ts
+++ src/ts/interact/Movement.ts
@@ -3,45 +3,31 @@ import { Actor } from "../actors/Actor";
 import { Map } from "../environment/Map";
 
 export class Movement {
-    static moveRandomly(area: Map, actor: Actor, distance: number) {
-        let prevPos: number[] = actor.position;
-        let posX: number = prevPos[0] + Math.floor(Utils.range(distance * -1, distance));
-        let posY: number = prevPos[1] + Math.floor(Utils.range(distance * -1, distance));
-        actor.position = [posX, posY];
-        if (posX > area.width / 2 || posX < area.width * -1 / 2) {
-            this.moveRandomly(area, actor, distance);
-        }
-        if (posY > area.height / 2 || posY < area.height * -1 / 2) {
-            this.moveRandomly(area, actor, distance);
-        }
+    static moveRandomly(area: Map, actor: Actor, distance: number): void {
+        actor.position[0] += Math.floor(Utils.range(-distance, distance));
+        actor.position[1] += Math.floor(Utils.range(-distance, distance));
     }
 
-    static randomPosition(area: Map, range: number) {
+    static randomPosition(area: Map, range: number): number[] {
         return [Math.floor(Utils.range(range * -1, range)), Math.floor(Utils.range(range * -1, range))];
     }
 
-    static move(area: Map, actor: Actor, direction: string, distance: number) {
-        let previousPos = actor.position;
-        switch (direction) {
-            case "N":
-                actor.position = [previousPos[0], previousPos[1] + distance];
-                break;
-            case "E":
-                actor.position = [previousPos[0] + distance, previousPos[1]];
-                break;
-            case "S":
-                actor.position = [previousPos[0], previousPos[1] - distance];
-                break;
-            case "W":
-                actor.position = [previousPos[0] - distance, previousPos[1]];
-                break;
+    static move(area: Map, actor: Actor, direction: string, distance: number): void {
+        if (direction === "N") {
+            actor.position[1] += distance;
+        } else if (direction === "E") {
+            actor.position[0] += distance;
+        } else if (direction === "S") {
+            actor.position[1] -= distance;
+        } else if (direction === "W") {
+            actor.position[0] -= distance;
         }
     }
 
-    static moveTo(actor: Actor, target: number[], distance: number) {
-        let Angle = Math.atan2(target[1] - actor.position[1], target[0] - actor.position[0]);
-        let Sin = Math.sin(Angle) * distance;
-        let Cos = Math.cos(Angle) * distance;
+    static moveTo(actor: Actor, target: number[], distance: number): void {
+        const Angle: number = Math.atan2(target[1] - actor.position[1], target[0] - actor.position[0]),
+            Sin: number = Math.sin(Angle) * distance,
+            Cos: number = Math.cos(Angle) * distance;
         actor.position[0] += Math.floor(Cos);
         actor.position[1] += Math.floor(Sin);
     }
